31cbc752e4572faddd552e80ab794936a010c5c8
Jesse Brooks <brooks.jesse83@gmail.com>
2022-03-15 20:38:56 -0700

    splash page styling

---
+++ react-app/src/components/SplashPage/index.js
@@ -0,0 +1,39 @@
+import './SplashPage.css';
+
+function SplashPage(){
+    return (
+        <>
+        <div className='top-bar-container'>
+            <div className='new-project-button'>
+
+            </div>
+            <div className='my-projects-button'>
+
+            </div>
+            <div className='home'>
+                
+            </div>
+        </div>
+        <div className='middle-content-container'>
+            <div className='logo-container'>
+                <div className='prjct-logo-text'>
+                    Prjct
+                </div>
+                <div className='plnnr-logo-text'>
+                    Plnnr
+                </div>
+                <div className='slogan-text'>
+                    for all your planning needs
+                </div>
+            </div>
+            <div className='splash-image'>
+                {/* <image>
+
+                </image> */}
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default SplashPage;

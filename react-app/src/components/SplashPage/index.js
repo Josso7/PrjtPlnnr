57b437c59c4a263226b6e4cde5df212fe6722c90
Jesse Brooks <brooks.jesse83@gmail.com>
2022-03-15 21:34:19 -0700

    splash styling

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,19 +1,37 @@
 import './SplashPage.css';
+import SplashImage from '../../images/todo-image.png';
+import { NavLink } from 'react-router-dom';
 
 function SplashPage(){
     return (
         <>
         <div className='top-bar-container'>
             <div className='new-project-button'>
-
+            {
+            // this will open a modal
+            }
+                NEW PROJECT
             </div>
+
+            <NavLink className='projects-button' to='/projects'>
             <div className='my-projects-button'>
+                MY PROJECTS
+            </div>
+            </NavLink>
 
+            <NavLink className='signup-button' to='/signup'>
+            <div className='signup-text'>
+                SIGN UP
             </div>
-            <div className='home'>
-                
+            </NavLink>
+
+            <NavLink className='login-button' to='/login'>
+            <div className='login-text'>
+                LOG IN
             </div>
+            </NavLink>
         </div>
+
         <div className='middle-content-container'>
             <div className='logo-container'>
                 <div className='prjct-logo-text'>
@@ -26,10 +44,12 @@ function SplashPage(){
                     for all your planning needs
                 </div>
             </div>
-            <div className='splash-image'>
-                {/* <image>
 
-                </image> */}
+            <div className='splash-image-container'>
+                <img
+                className='splash-image'
+                src={SplashImage}>
+                </img>
             </div>
         </div>
         </>

22ce0943a722887e276947c30e96acdebcf0daa7
Jesse Brooks <brooks.jesse83@gmail.com>
2022-03-16 11:19:32 -0700

    registered project blueprint

--- app/api/project_routes.py
+++ app/api/project_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request
 from flask_login import login_required
-from app.models import Project, User, db
+from app.models import Project, db
 
 project_routes = Blueprint('projects', __name__)
 

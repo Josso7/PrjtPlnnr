3ffb672a23f426e68e54e1245a50d5f85ad675cf
Jesse Brooks <brooks.jesse83@gmail.com>
2022-03-16 22:57:20 -0700

    added progress bar

--- react-app/src/components/ProjectChannels/index.js
+++ react-app/src/components/ProjectChannels/index.js
@@ -19,9 +19,29 @@ function ProjectChannels({ activeProject }){
     return(
         <>
             <div className='channels-container'>
-                <div className='selected-project-text'>
-                    {projects && activeProject && projects?.find(project => project.id == activeProject).name}
+                <div className='username-text'>
+                    {user.username}
                 </div>
+                <div class="dashboard">
+                <div className='progress-bar-wrapper'>
+                    <svg>
+                        <circle class="bg" cx="57" cy="57" r="52" />
+                        <circle class="meter-2" cx="57" cy="57" r="52" />
+                    </svg>
+                </div>
+                </div>
+                <div className='selected-project-container'>
+
+                    <div className='selected-project-text'>
+                        {projects && activeProject && projects?.find(project => project.id == activeProject).name}
+                    </div>
+
+                    <div className='add-channel-button'>
+                        +
+                    </div>
+
+                </div>
+
                 {channels && channels.map(channel => (
                 <div
                 key={channel.id}

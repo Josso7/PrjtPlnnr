e21992f1f6eb1cc9682052a2b4b6d149aae01f32
Jesse Brooks <brooks.jesse83@gmail.com>
2023-06-07 12:43:01 -0700

    removed second react_root func

--- app/__init__.py
+++ app/__init__.py
@@ -80,14 +80,7 @@ def react_root(path):
 
 if __name__ == '__main__':
     socketio.run(app)
-    
+
 @app.errorhandler(404)
 def not_found(e):
-    return app.send_static_file('index.html')
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def react_root(path):
-    if path == 'favicon.ico':
-        return app.send_from_directory('public', 'favicon.ico')
     return app.send_static_file('index.html')
